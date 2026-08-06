67e8219443db0448da1bd724be65c0326857961c
Soumiya-33 <soumiyasunilkumar33@gmail.com>
2026-08-06 20:06:17 +0530

    implement JWT auth with  mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+
 const uploadRoutes = require("./routes/upload");
 const extractRoutes = require("./routes/extract");
-
+const authRoutes = require("./routes/authRoutes");
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose"); const candidateRoutes = require("./routes/candidates");
 
-const candidateRoutes = require("./routes/candidates");
-
-require("dotenv").config();
+require("dotenv").config({ path: "./.env" });
 
 const app = express();
 
@@ -15,6 +18,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
 
 
 
@@ -30,6 +34,8 @@ app.use("/api", uploadRoutes);
 app.use("/api", extractRoutes);
 app.use("/api", candidateRoutes);
 app.use("/match", matchRoute);
+app.use("/api/auth", authRoutes);
+
 
 
 
@@ -44,10 +50,21 @@ app.use((err, req, res, next) => {
         })
     })
 });
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-
+app.get("/test", (req, res) => {
+    res.send("Working");
 });
+console.log(process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI, {
+    serverSelectionTimeoutMS: 10000,
+})
+    .then(() => {
+        console.log("MongoDB Connected");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB Connection Error:", err);
+    });
